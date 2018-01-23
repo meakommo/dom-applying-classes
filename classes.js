33a5cd400c8d11f0845d9f61d90aed3944077fd8
meako <meako98@gmail.com>
2018-01-23 13:45:42 +1300

    added css and js

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
   one()
   two()
   three()
+  makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -36,3 +37,8 @@ function three () {
   three.classList.add('red')
 }
 // CREATE FUNCTION makeVisible HERE
+
+function makeVisible () {
+	var invisible =	document.getElementsByClassName('invisible')[0]
+	invisible.classList.add('visible')
+}
